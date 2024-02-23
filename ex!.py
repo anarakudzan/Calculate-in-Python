0d968344f76c6e2481d69a0b8cacb079980c887d
anarakudzan <vladrenzin010@gmail.com>
2024-02-23 16:03:32 +0700

    calcPy

---
+++ ex!.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+znak = input("Выберите знак: ")
+if(znak == "+"):
+    print(num1 + num2)
+elif(znak == "-"):
+    print(num1 - num2)
+elif(znak == "*"):
+    print(num1 * num2)
+elif(znak == "/"):
+    print(num1 / num2)
